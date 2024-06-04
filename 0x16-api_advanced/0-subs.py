296e12973f4561e535aea071e06a5901f2be6855
Slimane Rhaytou <rhaytouslimane@gmail.com>
2024-06-04 23:28:38 +0000

    Commit message

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-""" script to obtain subscribers
-    count from a subreddit
 """
-from requests import get
+Query Reddit API and return the total number of subscribers
+for a given subreddit
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """ function to get subscriber count"""
-    if subreddit and type(subreddit) is str:
-        subscribers = 0
-        url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
-        req = get(url, headers=headers)
-        if req.status_code == 200:
-            data = req.json()
-            subscribers = data.get('data', {}).get('subscribers', 0)
-        return subscribers
+    """
+        get number of subscribers for a given subreddit
+        return 0 if invalid subreddit given
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
+    r = requests.get(url, headers=headers).json()
+    subscribers = r.get('data', {}).get('subscribers')
+    if not subscribers:
+        return 0
+    return subscribers
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,1 +1,1 @@
-# Documentation
+# Documentaion
